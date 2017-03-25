b029c6342495845b44064c4216a2b7ec19b8986a
Lucas Lafarga <elrutas@gmail.com>
2017-03-25 12:07:20 +0100

    Add more Karumies

--- src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
+++ src/test/java/com/karumi/maxibonkata/DeveloperProperties.java
@@ -13,27 +13,30 @@ import static org.junit.Assert.*;
 
     @Property public void maxibonsIsPositiveOrZero(int numberOfMaxibon) {
         Developer developer = new Developer("name", numberOfMaxibon);
-        System.out.println(developer);
+//        System.out.println(developer);
         assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
     }
 
     @Property public void maxibonsIsPositiveOrZeroForDevelopers(@From(DevelopersGenerator.class) Developer developer) {
-        System.out.println(developer);
+//        System.out.println(developer);
         assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
     }
 
     @Property public void maxibonsIsPositiveOrZeroForKarumies(@From(KarumiesGenerator.class) Developer developer) {
-        System.out.println(developer);
+//        System.out.println(developer);
         assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
     }
 
     @Property public void maxibonsIsPositiveOrZeroForNotSoHungry(@From(NotSoHungryDevelopersGenerator.class) Developer developer) {
-        System.out.println(developer);
+//        System.out.println(developer);
         assertTrue(developer.getNumberOfMaxibonsToGrab() >= 0);
     }
 
     @Test public void maxibonsAreTheExpectedNumber() {
         assertTrue(Karumies.ALBERTO.getNumberOfMaxibonsToGrab() == 1);
         assertTrue(Karumies.PEDRO.getNumberOfMaxibonsToGrab() == 3);
+        assertTrue(Karumies.DAVIDE.getNumberOfMaxibonsToGrab() == 0);
+        assertTrue(Karumies.JORGE.getNumberOfMaxibonsToGrab() == 1);
+        assertTrue(Karumies.SERGIO.getNumberOfMaxibonsToGrab() == 2);
     }
 }
